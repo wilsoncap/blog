508b71fcb207cfbc6e3ebc49a848d4888e90bce3
Wilson Capacho <wiston.98@hotmail.com>
2022-09-19 23:09:06 -0500

    mostrando datos de curso

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -2,25 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Curso;
 use Illuminate\Http\Request;
 
 class CursoController extends Controller
 {
     //por convencion al metodo principal se le suele llamar index
     public function index(){
-        return view('cursos.index');
+        $cursos = Curso::paginate();
+        return view('cursos.index', compact('cursos'));
     }
 
    
     //por convencion al metodo para mostrar la pagina para crear cualquier cosa, se le llama create
     public function create(){
-        return view('cursos.create');
+        return "crear curso";
     }
 
 
     //por convencion al metodo para mostrar la pagina para mostrar cualquier cosa, se le llama show
-    public function show($curso){
+    public function show($id){
         // otra  forma de parasa la variable compact('curso') que es igual a ['curso'=>$curso])
-        return view('cursos.show', compact('curso'));
+        return "curso seleccionado $id";
     }
 }
--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -5,6 +5,12 @@
 
 @section('content')
     <h1>Bienvenido a la pagina cursos</h1>
+    <ul>
+        @foreach ($cursos as $curso)
+            <li>{{$curso->name}}</li>
+        @endforeach
+    </ul>
+    {{ $cursos->links()}}
 @endsection
 
 
